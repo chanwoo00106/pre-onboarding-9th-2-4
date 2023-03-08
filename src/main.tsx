5cd6c71fba638ad848a73f06f061c71ba0c48627
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-03-08 17:56:03 +0900

    Begin project

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+)
