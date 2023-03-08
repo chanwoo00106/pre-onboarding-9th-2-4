3bd6f3cb8aa51c7d83c0bee1e19e2e5564ef2a16
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-03-08 20:22:07 +0900

    Fix api url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com',
+  baseURL: 'https://s3.us-west-2.amazonaws.com',
 })
 
 export default API
--- src/services/getProducts.ts
+++ src/services/getProducts.ts
@@ -4,7 +4,7 @@ import Product from '@/types/Product'
 const getProducts = async () => {
   try {
     const { data } = await API.get<Product[]>(
-      '/f1be87a4-38e1-4c1e-a527-bd4775812374/mock_data.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230307%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230307T102354Z&X-Amz-Expires=86400&X-Amz-Signature=555cc423722ff4558890f6b6b04e1dd7823e85b6c8adfc2f9798e2453cfaea94&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22mock_data.json%22&x-id=GetObject'
+      '/secure.notion-static.com/f1be87a4-38e1-4c1e-a527-bd4775812374/mock_data.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230308%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230308T111952Z&X-Amz-Expires=86400&X-Amz-Signature=02653a322ddaf764526a3013fe9da2617db3d2a108cd2de64e0947c9658ec010&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22mock_data.json%22&x-id=GetObject'
     )
 
     return data
